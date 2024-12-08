c26088baf910d633ce2cbd8c62f4fe62b69914d1
yun6160 <devyun6160@gmail.com>
2024-12-08 23:30:44 +0900

    feat: firebase setup

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import firebase from "firebaseApp";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
