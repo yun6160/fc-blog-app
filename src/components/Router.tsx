f8f109f93ee0bbf0e6759b3f87a74dd8e94d1b0f
yun6160 <devyun6160@gmail.com>
2024-12-19 00:19:43 +0900

    feat:loginForm, signupForm

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "../pages/home";
 import PostDetail from "../pages/posts/detail";
@@ -9,19 +11,31 @@ import SignupPage from "../pages/signup";
 import PostsPage from "../pages/posts";
 
 export default function Router() {
+    // firebase auth가 인증되었으면 true, 아니면 false 로직 추가
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
     return (
         <div>
             <Routes>
-                <Route path='/' element={<Home />}></Route>
-                <Route path='/posts' element={<PostsPage />}></Route>
-                <Route path='/posts/new' element={<NewPost />}></Route>
-                <Route path='/posts/:id' element={<PostDetail />}></Route>
-                <Route path='/posts/edit/:id' element={<EditPost />}></Route>
-                <Route path='/profile' element={<Profile />}></Route>
-                <Route path='/login' element={<LoginPage />}></Route>
-                <Route path='/signup' element={<SignupPage />}></Route>
-                {/**Navigate를 이용해서 지정하지 않은 path값이 들어올 시 강제로 홈으로 이동 */}
-                <Route path='*' element={<Navigate replace to='/'></Navigate>}></Route>
+                {isAuthenticated ? (
+                    <>
+                        <Route path='/' element={<Home />}></Route>
+                        <Route path='/posts' element={<PostsPage />}></Route>
+                        <Route path='/posts/new' element={<NewPost />}></Route>
+                        <Route path='/posts/:id' element={<PostDetail />}></Route>
+                        <Route path='/posts/edit/:id' element={<EditPost />}></Route>
+                        <Route path='/profile' element={<Profile />}></Route>
+                        <Route path='/login' element={<LoginPage />}></Route>
+                        <Route path='/signup' element={<SignupPage />}></Route>
+                        {/**Navigate를 이용해서 지정하지 않은 path값이 들어올 시 강제로 홈으로 이동 */}
+                        <Route path='*' element={<Navigate replace to='/'></Navigate>}></Route>
+                    </>
+                ) : (
+                    <>
+                        <Route path='/login' element={<LoginPage />} />
+                        <Route path='/signup' element={<SignupPage />} />
+                        <Route path='*' element={<LoginPage />} />
+                    </>
+                )}
             </Routes>
         </div>
     );
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,12 @@
+import Footer from "components/Footer";
+import Header from "components/Header";
+import LoginForm from "components/LoginForm";
+
 export default function LoginPage() {
-    return <h2>Login Page</h2>;
+    return (
+        <>
+            <Header />
+            <LoginForm />
+        </>
+    );
 }
