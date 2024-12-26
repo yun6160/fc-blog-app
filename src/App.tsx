e1013a5a24e5c6f4d4b407365cba9f673ce13a3d
yun6160 <devyun6160@gmail.com>
2024-12-27 00:25:25 +0900

    feat:firebase signup & toastify setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import { app } from "./firebaseApp";
 import { getAuth } from "firebase/auth";
+import { ToastContainer } from "react-toastify";
 import Router from "./components/Router";
 
 function App() {
     const auth = getAuth(app);
-    console.log(auth);
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!auth?.currentUser);
-    return <Router isAuthenticated={isAuthenticated} />;
+    return (
+        <>
+            <ToastContainer closeButton={false} />
+            <Router isAuthenticated={isAuthenticated} />
+        </>
+    );
 }
 
 export default App;
--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,20 +1,99 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { app } from "firebaseApp";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { toast } from "react-toastify";
 
 export default function SignupForm() {
+    const [errors, setErrors] = useState<{ email?: string; password?: string; passwordConfirm?: string }>({});
+
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [passwordConfirm, setPasswordConfirm] = useState<string>("");
+
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        try {
+            const auth = getAuth(app);
+            await createUserWithEmailAndPassword(auth, email, password);
+            toast.success("회원가입이 완료되었습니다🎉");
+        } catch (error: any) {
+            toast.error(error?.code);
+        }
+    };
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+
+        if (name === "email") {
+            setEmail(value);
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!emailRegex?.test(value)) {
+                setErrors((prevErrors) => ({ ...prevErrors, email: "이메일 형식이 올바르지 않습니다." }));
+            } else {
+                setErrors((prevErrors) => ({ ...prevErrors, email: "" }));
+            }
+        }
+        if (name === "password") {
+            setPassword(value);
+
+            if (value?.length < 8) {
+                setErrors((prevErrors) => ({ ...prevErrors, password: "비밀번호는 8자 이상이어야 합니다." }));
+            } else if (passwordConfirm?.length > 0 && value !== passwordConfirm) {
+                setErrors((prevErrors) => ({
+                    ...prevErrors,
+                    password: "비밀번호가 일치하지 않습니다. 다시 확인해주세요.",
+                }));
+            } else {
+                setErrors((prevErrors) => ({ ...prevErrors, password: "" }));
+            }
+        }
+        if (name === "password_confirm") {
+            setPasswordConfirm(value);
+
+            if (value?.length < 8) {
+                setErrors((prevErrors) => ({ ...prevErrors, passwordConfirm: "비밀번호는 8자 이상이어야 합니다." }));
+            } else if (value !== password) {
+                setErrors((prevErrors) => ({
+                    ...prevErrors,
+                    passwordConfirm: "비밀번호가 일치하지 않습니다. 다시 확인해주세요.",
+                }));
+            } else {
+                setErrors((prevErrors) => ({ ...prevErrors, passwordConfirm: "" }));
+            }
+        }
+    };
+
     return (
-        <form action='/post' method='POST' className='form form--lg'>
+        <form onSubmit={onSubmit} className='form form--lg'>
             <h1 className='form__title'>회원가입</h1>
             <div className='form__block'>
                 <label htmlFor='email'>이메일</label>
-                <input type='email' id='email' name='email' required />
+                <input type='email' id='email' name='email' required onChange={onChange} />
+                {errors.email && (
+                    <div className='form__block'>
+                        <div className='form__error'>{errors.email}</div>
+                    </div>
+                )}
             </div>
             <div className='form__block'>
                 <label htmlFor='password'>비밀번호</label>
-                <input type='password' id='password' name='password' required />
+                <input type='password' id='password' name='password' required onChange={onChange} />
+                {errors.password && (
+                    <div className='form__block'>
+                        <div className='form__error'>{errors.password}</div>
+                    </div>
+                )}
             </div>
             <div className='form__block'>
                 <label htmlFor='password_confirm'>비밀번호 확인</label>
-                <input type='password' id='password_confirm' name='password_confirm' required />
+                <input type='password' id='password_confirm' name='password_confirm' required onChange={onChange} />
+                {errors.passwordConfirm && (
+                    <div className='form__block'>
+                        <div className='form__error'>{errors.passwordConfirm}</div>
+                    </div>
+                )}
             </div>
             <div className='form__block'>
                 계정이 이미 있으신가요?
@@ -23,7 +102,12 @@ export default function SignupForm() {
                 </Link>
             </div>
             <div className='form__block'>
-                <input type='submit' value='회원가입' className='form__btn--submit' />
+                <input
+                    type='submit'
+                    value='회원가입'
+                    className='form__btn--submit'
+                    disabled={Object.values(errors).some((error) => error.length > 0)}
+                />
             </div>
         </form>
     );
